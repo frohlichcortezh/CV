577184abae097eb2db6335f7d71fb8cf528eb4c7
Hector Frohlich Cortez <frohlichcortezh@protonmail.com>
2020-03-03 13:22:26 +0100

    Update on working dates

--- public/js/print.js
+++
@@ -1,6 +0,0 @@
-window.addEventListener('beforeprint', (event) => {
-    [...acc].forEach(function(elem) {
-        elem.classList.toggle("active", true);
-        elem.nextElementSibling.style.maxHeight = elem.nextElementSibling.scrollHeight + "px";
-    });    
-  });
