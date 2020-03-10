9f038af575c218300a2f63860b77a1edf8b98d66
Hector Frohlich Cortez <frohlichcortezh@protonmail.com>
2020-03-10 21:53:35 +0100

    Update skills

--- public/js/print.js
+++
@@ -1,9 +0,0 @@
-var _print = window.print;
-
-window.print = function() {
-    [...acc].forEach(function(elem) {
-        elem.classList.toggle("active", true);
-        elem.nextElementSibling.style.maxHeight = elem.nextElementSibling.scrollHeight + "px";
-    });
-    _print();
- }
